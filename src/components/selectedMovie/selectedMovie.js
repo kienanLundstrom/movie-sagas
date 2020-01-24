bcddc716c5792017037fb25aedcb35cfafe324fa
Kienan Lundstrom <keen.lundy@gmail.com>
2020-01-24 13:31:57 -0600

    added material ui to display information on homepage and for selected movies

--- src/components/selectedMovie/selectedMovie.js
+++ src/components/selectedMovie/selectedMovie.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import { Grid, Paper } from '@material-ui/core';
+import './SelectedMovie.css';
+
 
 class SelectedMovie extends Component {
     componentDidMount() {
@@ -15,7 +18,27 @@ class SelectedMovie extends Component {
     render() {
         return(
             <div className="selectedMovie">
-
+                <Grid container justify='center' spacing={32}>
+                    <Paper height="100%">
+                        {this.props.reduxState.movie.map((movie)=>{
+                            return(
+                                <Grid key={movie.id} item xs={12}>
+                                    <img src={movie.poster}/>
+                                    <h1>{movie.title}</h1>
+                                    <Grid>
+                                        <p>{movie.description}</p>
+                                    </Grid>
+                                </Grid>
+                            )
+                        })}
+                         <h3>Genres:</h3>
+                            {this.props.reduxState.genres.map((genre, i) => {
+                                return (
+                                    <p key={i}>{genre.name}</p>
+                                )
+                            })}
+                    </Paper>
+                </Grid>
             </div>
         )
     }
